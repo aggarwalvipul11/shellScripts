fd1bcad08b0e2f65b51698187b64bbe3561912b8
aggarwalvipul11 <vipul_dev@DESKTOP-QA3S37M.localdomain>
2020-06-05 13:35:25 +0530

    Update Employee Attendence.

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-echo "Welcome to employee wage computation "
+echo "Welcome to Employee Wage Computation."
+if [[ $(($RANDOM%2)) -eq 1 ]]
+then
+        echo "EMPLOYEE IS ABSENT "
+else
+        echo "EMPLOYEE IS PRESENT"
+fi
