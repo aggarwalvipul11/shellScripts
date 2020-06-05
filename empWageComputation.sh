9706b867815be93de87ee50b8d197dfcbf803982
aggarwalvipul11 <vipul_dev@DESKTOP-QA3S37M.localdomain>
2020-06-05 14:11:38 +0530

    Completed Solving Employee Hour and Wage Using Case Statment.

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,45 +1,18 @@
 #!/bin/bash
 echo "Welcome to Employee Wage Computation."
 
-# CONSTANTS FOR THE PROGRAM
-IS_PART_TIME=1;
-IS_FULL_TIME=2;
-MAX_HRS_IN_MONTH=100;
-EMP_RATE_PER_HR=20;
-NUM_WORKING_DAYS=20;
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+empCheck=$((RANDOM%3));
 
-# VARIABLES
-totalEmpHr=0;
-totalWorkingDays=0;
+case $empCheck in
+	$isFullTime)
+		empHrs=8;;
+	$isPartTime)
+		empHrs=4;;
+	*)
+		empHrs=0;;
+esac
 
-declare -A dailyWage;
-
-function getWorkHrs() {
-	local $empCheck=$1
-	case $empCheck in
-      $IS_FULL_TIME)
-         empHrs=8;;
-      $IS_PART_TIME)
-         empHrs=4;;
-      *)
-         empHrs=0;;
-   esac
-	echo $empHrs
-}
-
-function getEmpWage() {
-	local empHr=$1
-	echo $(($empHr*$EMP_RATE_PER_HR))
-}
-while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
-do
-	((totalWorkingDays++))
-	empCheck=$((RANDOM%3));
-	empHrs="$( getWorkHrs $empCheck )"
-	totalEmpHrs=$(($totalEmpHrs+$empHrs))
-	dailyWage["Day" $totalWorkingDays]="$( getEmpWage $empHrs )"
-done
-
-totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR));
-echo ${dailyWage[@]}
-echo ${!dailyWage[@]}
+salary=$(($empHrs*$empRatePerHr));
